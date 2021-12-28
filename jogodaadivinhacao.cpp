8e86b6d3e42c450c067e492e30d647527dd882b6
Ricardo Hatsugai <rhatsugai@gmail.com>
2021-12-28 20:51:34 -0300

    Aprendendo a usar o cin e cout

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -5,8 +5,22 @@ using namespace std;
 int main(){
     cout << "**********************************************" << endl;
     cout << "************ Seja bem vindo ******************" << endl;
-    cout << "**********************************************" << endl;
+    cout << "**********************************************" << endl << endl;
 
     int numero_secreto = 32;
-    cout << "O número secreto é " << numero_secreto << ". Não conte para ninguém!" << endl;
+
+    int chute=0;
+    cout << "Qual seu chute? " << endl;
+    cin >> chute;
+    cout << "O valor do seu chute é: " << chute << endl;
+
+    if(chute == numero_secreto){
+        cout << "Parabéns! Você acertou o número secreto!" << endl;
+    }
+    else if(chute > numero_secreto){
+        cout << "Seu chute foi maior que o número secreto!" << endl;
+    }
+    else{
+        cout << "Seu chute foi menor que o número secreto!" << endl;
+    }
 }
