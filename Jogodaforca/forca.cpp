9018807423e4e2f7dca781dc7689116b14b683cf
Ricardo Hatsugai <rhatsugai@gmail.com>
2021-12-29 13:07:28 -0300

    Condições de final de jogo

--- Jogodaforca/forca.cpp
+++ Jogodaforca/forca.cpp
@@ -18,18 +18,26 @@ bool letra_existe(char chute){
     return false;    
 }
 
+bool nao_acertou(){
+    for(char letra : PALAVRA_SECRETA){
+        if(!chutou[letra]){
+            return true;
+        }
+    }
+    return false;
+}
 
+bool nao_enforcou(){
+    return chutes_errados.size() < 5;
+}
 
 int main(){
 
     cout << "**********************" << endl;
     cout << "*** Jogo da Forca ***" << endl;
     cout << "**********************" << endl << endl;
-    
-    bool nao_acertou = true;
-    bool nao_enforcou = true;
 
-    while (nao_acertou && nao_enforcou){
+    while (nao_acertou() && nao_enforcou()){
 
         cout << "Chutes errados:";
         for(char letra: chutes_errados){
