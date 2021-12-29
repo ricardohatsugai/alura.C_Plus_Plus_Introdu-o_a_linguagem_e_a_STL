b6d0d64260081e7b6d8985834bff11b41f173ee6
Ricardo Hatsugai <rhatsugai@gmail.com>
2021-12-28 21:07:38 -0300

    Novos tipos em C++

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -7,17 +7,21 @@ int main(){
     cout << "************ Seja bem vindo ******************" << endl;
     cout << "**********************************************" << endl << endl;
 
-    int numero_secreto = 32;
-
+   /*Declarações de variáveis*/
+    const int numero_secreto = 32; // Variável constante.
     int chute=0;
+    bool acertou = chute == numero_secreto;
+    bool maior = chute > numero_secreto;
+    /****************************/
+
     cout << "Qual seu chute? " << endl;
     cin >> chute;
     cout << "O valor do seu chute é: " << chute << endl;
 
-    if(chute == numero_secreto){
+    if(acertou){
         cout << "Parabéns! Você acertou o número secreto!" << endl;
     }
-    else if(chute > numero_secreto){
+    else if(maior){
         cout << "Seu chute foi maior que o número secreto!" << endl;
     }
     else{
