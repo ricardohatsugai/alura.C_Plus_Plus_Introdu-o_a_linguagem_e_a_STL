328fe2a53f4e686b9ef6b558dbb8208701e04377
Ricardo Hatsugai <rhatsugai@gmail.com>
2021-12-29 12:41:11 -0300

    Escrevendo a palavra secreta

--- Jogodaforca/forca.cpp
+++ Jogodaforca/forca.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA";
+map<char, bool> chutou;
 
 bool letra_existe(char chute){
     for (char letra : PALAVRA_SECRETA){
@@ -14,16 +16,26 @@ bool letra_existe(char chute){
     return false;    
 }
 
+
 int main(){
-    cout << PALAVRA_SECRETA << endl;
     
     bool nao_acertou = true;
     bool nao_enforcou = true;
 
-    while (nao_acertou && nao_enforcou)
-    {
+    while (nao_acertou && nao_enforcou){
+        for(char letra : PALAVRA_SECRETA){
+            if(chutou[letra]){
+                cout << letra << " ";
+            }else{
+                cout << "_ ";
+            }
+        }
+        cout << endl;
+
         char chute;
         cin >> chute;
+
+        chutou[chute] = true;
 
         if (letra_existe(chute)){
             cout << "Você acertou! Seu chute está na palavra." << endl;
