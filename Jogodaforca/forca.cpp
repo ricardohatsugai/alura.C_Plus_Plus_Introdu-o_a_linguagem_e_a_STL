656d16d7ff4f29274605be8fadfc0b5f6d9cefcc
Ricardo Hatsugai <rhatsugai@gmail.com>
2021-12-30 09:29:44 -0300

    Adicionando palavras ao banco

--- Jogodaforca/forca.cpp
+++ Jogodaforca/forca.cpp
@@ -111,6 +111,35 @@ void sorteia_palavra(){
     palavra_secreta = palavras[indice_sorteado];
 }
 
+void salva_arquivo(vector<string> nova_lista){
+    ofstream arquivo;
+    arquivo.open("palavras.txt");
+    if(arquivo.is_open()){
+        arquivo << nova_lista.size() << endl;
+
+        for(string palavra : nova_lista){
+            arquivo << palavra << endl;
+        }
+
+        arquivo.close();
+
+    }else{
+        cout << "Não foi possível acessar o banco de palaras." << endl;
+        exit(0);
+    }
+}
+
+void adiciona_palavra(){
+    cout << "Digite a nova palavra, usando letras maiúsculas." << endl;
+    string nova_palavra;
+    cin >> nova_palavra;
+
+    vector<string> lista_palavras = le_arquivo();
+    lista_palavras.push_back(nova_palavra);
+
+    salva_arquivo(lista_palavras);
+}
+
 int main(){
 
     imprime_cabecalho();
@@ -133,6 +162,13 @@ int main(){
         cout << "Você perdeu! Tente novamente!" << endl;
     }else{
         cout << "Parabéns! Vocẽ acertou a palavra secreta!" << endl;
+        
+        cout << "Você deseja adicionar uma nova palavra ao banco? (S/N) ";
+        char resposta;
+        cin >> resposta;
+        if(resposta == 'S'){
+            adiciona_palavra();
+        }
     }
     
     
