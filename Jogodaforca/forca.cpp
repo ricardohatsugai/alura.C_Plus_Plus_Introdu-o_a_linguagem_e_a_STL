c8ab8d73372eadf462836f4ad02a5c6cc76b201b
Ricardo Hatsugai <rhatsugai@gmail.com>
2021-12-30 09:09:43 -0300

    Problemas com arquivos

--- Jogodaforca/forca.cpp
+++ Jogodaforca/forca.cpp
@@ -79,18 +79,26 @@ vector <string> le_arquivo(){
     ifstream arquivo;
     arquivo.open("palavras.txt");
 
-    int quantidade_palavras;
-    arquivo >> quantidade_palavras;
+    if(arquivo.is_open()){
+        int quantidade_palavras;
+        arquivo >> quantidade_palavras;
 
-    vector<string> palavras_do_arquivo;
+        vector<string> palavras_do_arquivo;
 
-    for (int i = 0; i < quantidade_palavras; i++)
-    {
-        string palavra_lida;
-        arquivo >> palavra_lida;
-        palavras_do_arquivo.push_back(palavra_lida);
+        for (int i = 0; i < quantidade_palavras; i++)
+        {
+            string palavra_lida;
+            arquivo >> palavra_lida;
+            palavras_do_arquivo.push_back(palavra_lida);
+        }
+
+        arquivo.close();
+
+        return palavras_do_arquivo;
+    }else{
+        cout << "Não foi possível acessar o banco de palavras." << endl;
+        exit(0);
     }
-    return palavras_do_arquivo;
     
 }
 
